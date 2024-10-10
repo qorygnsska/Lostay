144c0bbecfd9342cde934196936d78595e05b9a6
qorygnsska <qorygnsska@naver.com>
2024-10-10 18:36:43 +0900

    배교훈

--- frontend/src/pages/Hotel/HotelList.jsx
+++ frontend/src/pages/Hotel/HotelList.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import './HotelList.css';
 
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { FaStar } from "react-icons/fa6";
 
 export default function hotelList() {
 
@@ -14,10 +13,16 @@ export default function hotelList() {
           <div className='HotelCol'>
             <img src='/HotelList/호텔1.jpg' className='HotelImg'/>
             <div className='HotelInfo'>
-              <p>등급분류</p>
-              호텔명
-              위치
-              후기평점
+              <div className='HotelRank'>블랙·5성급·호텔</div>
+              <div className='HotelName'>제주신라호텔</div>
+              <div className='HotelLo'>서귀포시</div>
+              <div className='StarDiv'>
+                <div className='Star'>
+                  <FaStar className='StarIcon'/>
+                   <div className='StarNum'>9.6</div>
+                </div>
+                <span>901명 평가</span>
+              </div>
             </div>
             <div className='HotelPrice'>
               411,400원
