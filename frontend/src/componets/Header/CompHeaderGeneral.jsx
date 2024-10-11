e43e0269bd22676a3cc6324aa3310fd5fe54be57
qwer5105 <qqww5105@gmail.com>
2024-10-11 16:59:20 +0900

    정일 수정

---
+++ frontend/src/componets/Header/CompHeaderGeneral.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CompHeaderGeneral() {
+    return (
+        <>
+            <div className='comp--header--general--container'>
+                <p>CompHeaderGeneral</p>
+            </div>
+        </>
+
+    )
+}
---
+++ frontend/src/pages/Event/PageEventDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function PageEventDetail() {
+    return (
+        <>
+            <div className='page--event--detail--container'>
+                <h1>PageEventDetail</h1>
+            </div>
+
+        </>
+    )
+}
