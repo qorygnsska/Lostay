a46ae5008150510955140c3b4c5d6c878e0fb85f
jh960101 <ghdgns82@gmail.com>
2024-10-14 19:28:56 +0900

    mainController,service생성

--- backend/src/main/java/com/lostay/backend/main/service/MainService.java
+++ backend/src/main/java/com/lostay/backend/main/service/MainService.java
@@ -1,5 +1,23 @@
 package com.lostay.backend.main.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lostay.backend.hotel.dto.HotelDTO;
+import com.lostay.backend.hotel.repository.HotelRepository;
+
+@Service
 public class MainService {
+
+	@Autowired
+	private HotelRepository hotelRepo;
+	
+	public List<HotelDTO> hotels() {
+		
+	
+		return null;
+	}
 
 }
