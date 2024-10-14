04c48d9c16cf1631f4158173b84e68958f17fb84
qwer5105 <qqww5105@gmail.com>
2024-10-15 00:37:43 +0900

    JIP admin

--- frontend/src/pages/Admin/PageReviewManager.jsx
+++ frontend/src/pages/Admin/PageReviewManager.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
 import CompHeaderAdmin from '../../componets/Header/CompHeaderAdmin'
+import { Container, Table } from 'react-bootstrap'
+import CompAdminSearch from '../../componets/Admin/CompAdminSearch'
+import CompAdminBtn from '../../componets/Admin/CompAdminBtn'
 
 export default function PageReviewManager() {
     return (
         <>
-            <div className='page--review--manager--container'>
+            <div className='page--review--manager--container page--admin'>
                 <CompHeaderAdmin />
-                <h1>PageReviewManager</h1>
-            </div>
 
+                <Container id='section_container'>
+
+                    <div className='d-flex justify-content-end mb-3'>
+                        <CompAdminSearch/>
+                    </div>
+
+                    <Table striped bordered hover id='table_entire_review'>
+                        <thead id="table_header">
+                            <tr>
+                                <th>#</th>
+                                <th>리뷰 내용</th>
+                                <th>작성자</th>
+                                <th>작성일</th>
+                                <th>관리</th>
+                            </tr>
+                        </thead>
+                        <tbody id="table_body">
+                            <tr>
+                                <td>1</td>
+                                <td>이벤트 내용</td>
+                                <td>작성자1</td>
+                                <td>24.10.15.</td>
+                                <td className='btn_container'>
+                                    <CompAdminBtn >삭제</CompAdminBtn>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+
+                </Container>
+            </div>
         </>
     )
 }
