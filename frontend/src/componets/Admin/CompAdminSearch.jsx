bf5e40b1b42955733a59879c0a996477eed90f50
qwer5105 <qqww5105@gmail.com>
2024-10-15 16:29:05 +0900

    JIP comp page admin

--- frontend/src/componets/Admin/CompAdminSearch.jsx
+++ frontend/src/componets/Admin/CompAdminSearch.jsx
@@ -1,28 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { CiSearch } from 'react-icons/ci'
 
-export default function CompAdminSearch() {
+export default function CompAdminSearch(props) { //props: 상위요소에게서 받아온 유산
 
-  const handleSearch = () => {
-    
-    console.log('click_btn_search');
-    
+  //내가(하위 요소) 만들어서 쓸 변수
+  const [text_search, setText_search] = useState('');
+
+
+  const handleKey = (event) => {  //엔터키 누르면 #btn_search 클릭 실행
+    //console.log(event.target.value);
+    if(event.key === 'Enter') {
+      handleClick();  
+    }
+  }
+
+  const handleSubmit = (event) => { //엔터키 누르면 제출(page reload) 방지
+    event.preventDefault();   
+  }
+
+
+  const handleClick = () => {
+    console.log('click_btn_search //where: ' + props.where);  
+
+    if (text_search == '') {
+      handleNull();
+    } else {
+      handleSearch();
+    }
+    //내가 할 꺼 다 하고
+
+    //상위요소에게 값을 넘겨주기 위해 상위요소를 부름
+    props.callParent(text_search)
+
+  }
+
+
+  const handleNull = () => {  //모든 값 가져오기?
+    console.log('input_nothing');
+  }
+
+  const handleSearch = () => {  //검색 필터링
+    console.log('text_search: ' + text_search);
   }
 
 
   return (
     <>
-        <Form className="comp--admin--search--container">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-              id="input_search"
-            />
-            <Button id="btn_search" onClick={handleSearch} variant="outline-primary" size="sm"><CiSearch size="24" /></Button>
-          </Form>
+      <Form className="comp--admin--search--container" onKeyUp={handleKey} onSubmit={handleSubmit} >
+        <Form.Control
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+          id="input_search"
+          value={text_search}
+          onChange={(event) => { setText_search(event.target.value) }}
+        />
+        <Button id="btn_search" onClick={handleClick} variant="outline-primary" size="sm"><CiSearch size="24" /></Button>
+        {/* onClick={text_search!=''?handleSearch:null} */}
+      </Form>
     </>
   )
 }
--- frontend/src/pages/Admin/PageReviewManager.jsx
+++ frontend/src/pages/Admin/PageReviewManager.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CompHeaderAdmin from '../../componets/Header/CompHeaderAdmin'
 import { Container, Table } from 'react-bootstrap'
 import CompAdminSearch from '../../componets/Admin/CompAdminSearch'
@@ -6,13 +6,22 @@ import CompAdminBtn from '../../componets/Admin/CompAdminBtn'
 
 export default function PageReviewManager() {
 
-    
-    function deleteReview() {   //'삭제' 버튼에 상속해줄 함수
-        console.log('click_delete-review');
-        
+    //하위요소가 넘겨줄 값을 담을 변수
+    const [text_fromChild, setText_fromChild] = useState('');
+
+    //하위요소가 값을 넘겨주면 실행할 함수
+    const functionForMyChild = (fromMyChild) => {
+        //fromMyChild: 하위요소가 넘겨준 변수(text_search)의 매개변수
+        console.log('text_fromChild: ' + text_fromChild);   //previousState
+        console.log('fromChild: ' + fromMyChild);
+        setText_fromChild(fromMyChild);
     }
 
 
+    function deleteReview() {   //하위요소 '삭제' 버튼에 상속해줄 함수
+        console.log('click_delete_review');
+        
+    }
 
 
     return (
@@ -22,8 +31,10 @@ export default function PageReviewManager() {
 
                 <Container id='section_container'>
 
-                    <div className='d-flex justify-content-end mb-3'>
-                        <CompAdminSearch/>
+                    <div className='d-flex justify-content-between mb-3'>
+                        <p>하위컴포넌트(검색창)가 준 값: {text_fromChild}</p>
+                        {/* 하위요소를 생성하면서, 상위요소를 부르면 실행할 함수명을 지정 */}
+                        <CompAdminSearch where={'admin-review'} callParent={functionForMyChild} />
                     </div>
 
                     <Table striped bordered hover id='table_entire_review'>
