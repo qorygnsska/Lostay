6d36177b1135c344913d22f8908eef004f44ae3c
qwer5105 <qqww5105@gmail.com>
2024-10-15 09:44:30 +0900

    Apply .gitignore

--- frontend/src/componets/Admin/CompAdminBtn.jsx
+++ frontend/src/componets/Admin/CompAdminBtn.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
 
-export default function CompAdminBtn({children}) {
+export default function CompAdminBtn({children}, props) {
+
+
+
+
 
     //children: 상속 넘겨줄 것!==innerHTML
     //<CompAdminBtn>'children'</CompAdminBtn>
 
   return (  //Customized Button
     <>
-        <button className='comp--admin--btn--container'>{children}</button>
+        <button className='comp--admin--btn--container' onClick={props.click}>{children}</button>
     </>
   )
 }
