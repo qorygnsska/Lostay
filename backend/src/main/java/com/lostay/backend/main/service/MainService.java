d4c19922a8202492b7e440720753e8c8d6c1e284
jh960101 <ghdgns82@gmail.com>
2024-10-15 09:44:54 +0900

    locationDB화면출력

--- backend/src/main/java/com/lostay/backend/main/service/MainService.java
+++ backend/src/main/java/com/lostay/backend/main/service/MainService.java
@@ -1,5 +1,6 @@
 package com.lostay.backend.main.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,17 +8,35 @@ import org.springframework.stereotype.Service;
 
 import com.lostay.backend.hotel.dto.HotelDTO;
 import com.lostay.backend.hotel.repository.HotelRepository;
+import com.lostay.backend.location.dto.LocationDTO;
+import com.lostay.backend.location.entity.Location;
+import com.lostay.backend.location.repository.LocationRepository;
 
 @Service
 public class MainService {
 
 	@Autowired
 	private HotelRepository hotelRepo;
+	@Autowired
+	private LocationRepository locationRepo;
 	
 	public List<HotelDTO> hotels() {
 		
 	
 		return null;
+	}
+
+	public List<LocationDTO> findAll() {
+		List<Location> locationentity= locationRepo.findAll();
+		List<LocationDTO>locationDTOList= new ArrayList<LocationDTO>();
+		for(Location location:locationentity) {
+			LocationDTO dto= new LocationDTO();
+			dto.setLocationImage(location.getLocationImage());
+			dto.setLocationName(location.getLocationName());
+			dto.setLocationNo(location.getLocationNo());
+			locationDTOList.add(dto);
+		}
+		return locationDTOList ;
 	}
 
 }
