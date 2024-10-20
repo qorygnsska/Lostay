2b147bf82758d788b48870d38eae573854f776fb
Hangma01 <dn09yooon@gmail.com>
2024-10-20 14:06:55 +0900

    예약 결제 페이지 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,66 +4,70 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login';
-import LoginPopup from './pages/Login/LoginPopup';
-import HotelList from './pages/Hotel/HotelList';
 
-import PageEventList from './pages/Event/PageEventList';                    //이벤트리스트 페이지
-import PageEventDetail from './pages/Event/PageEventDetail';                //이벤트상세 페이지
 
-import PageAdminHome from './pages/Admin/PageAdminHome';                    //관리자 홈 페이지
-import PageEventManager from './pages/Admin/PageEventManager';              //이벤트관리 페이지
-import PageReviewManager from './pages/Admin/PageReviewManager';            //리뷰관리 페이지
-import PageUserManager from './pages/Admin/PageUserManager';                //사용자관리 페이지
-import MyPage from './pages/MyPage/MyPage';                                 // 마이 페이지
-import WishList from './pages/MyPage/Wish/WishList';                        // 찜 목록 페이지
-import Profile from './pages/MyPage/Profile/Profile';                       // 내 정보 관리 페이지
-import BookingHistory from './pages/MyPage/BookingHistory/BookingHistory';  // 예내내역 페이지
-import RoomList from './pages/Hotel/RoomList';
-import HotelMap from './pages/Hotel/HotelMap';
-import Payment from './pages/MyPage/BookingHistory/Payment/Payment'         // 결제내역 페이지
-import MyReview from './pages/MyPage/MyReview/MyReview'
-import MyPoint from './pages/MyPage/MyPoint/MyPoint'
+// 효준
+import Home from './pages/Home/Home'                                        // 메인 페이지
+import Login from './pages/Login/Login';                                    // 로그인 페이지
+import LoginPopup from './pages/Login/LoginPopup';                          // 소셜 로그인 팝업 페이지
+import Payment from './pages/MyPage/BookingHistory/Payment/Payment'         // 결제 내역 페이지
+import MyPage from './pages/MyPage/MyPage';                                 // 마이페이지 - 메인
+import WishList from './pages/MyPage/Wish/WishList';                        // 마이페이지 - 찜 내역
+import Profile from './pages/MyPage/Profile/Profile';                       // 마이페이지 - 내 정보 관리
+import BookingHistory from './pages/MyPage/BookingHistory/BookingHistory';  // 마이페이지 - 예매 내역
+import MyReview from './pages/MyPage/MyReview/MyReview'                     // 마이페이지 - 리뷰 내역
+import MyPoint from './pages/MyPage/MyPoint/MyPoint'                        // 마이페이지 - 포인트 내역
+import Reservation from './pages/Reservation/Reservation'                   // 예약 결제 페이지
+
+
+// 교훈
+import HotelList from './pages/Hotel/HotelList';                            // 호텔 리스트 페이지
+import RoomList from './pages/Hotel/RoomList';                              // 객실 리스트 페이지
+import HotelMap from './pages/Hotel/HotelMap';                              // 지도 페이지
+
+
+// 정일
+import PageEventList from './pages/Event/PageEventList';                    // 이벤트리스트 페이지
+import PageEventDetail from './pages/Event/PageEventDetail';                // 이벤트상세 페이지
+import PageAdminHome from './pages/Admin/PageAdminHome';                    // 관리자 페이지 - 메인
+import PageEventManager from './pages/Admin/PageEventManager';              // 관리자 페이지 - 이벤트 관리
+import PageReviewManager from './pages/Admin/PageReviewManager';            // 관리자 페이지 - 리뷰 관리
+import PageUserManager from './pages/Admin/PageUserManager';                // 관리자 페이지 - 사용자 관리
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    //    errorElement: < />, // 404페이지
     children: [
+
+      // 효준
       { index: true, element: <Home /> },
       { path: '/login', element: <Login /> },
-      { path: '/hotelList', element: <HotelList /> },
       { path: '/login-popup', element: <LoginPopup /> },
+      { path: '/payment', element: <Payment /> },
+      { path: '/mypage', element: <MyPage /> },
+      { path: '/wishlist', element: <WishList /> },
+      { path: '/profile', element: <Profile /> },
+      { path: '/booking-history', element: <BookingHistory />},
+      { path: '/my-point', element: <MyPoint /> },
+      { path: '/my-review', element: <MyReview /> },
+      { path: '/reservation', element: <Reservation />},
+
+
+      // 교훈
+      { path: '/hotelList', element: <HotelList /> },
+      { path: '/roomList', element: <RoomList /> },
+      { path: '/hotelMap', element: <HotelMap /> },
+
+
+      // 정일
       { path: '/event-list', element: <PageEventList /> },
       { path: '/event-detail', element: <PageEventDetail /> },
       { path: '/admin-home', element: <PageAdminHome /> },
       { path: '/admin-event', element: <PageEventManager /> },
       { path: '/admin-review', element: <PageReviewManager /> },
       { path: '/admin-user', element: <PageUserManager /> },
-      { path: '/mypage', element: <MyPage /> },
-      { path: '/wishlist', element: <WishList /> },
-      { path: '/roomList', element: <RoomList /> },
-      { path: '/profile', element: <Profile /> },
-      { path: '/booking-history', element: <BookingHistory />, children: [] },
-      { path: '/hotelMap', element: <HotelMap /> },
-      { path: '/payment', element: <Payment /> },
-      { path: '/my-review', element: <MyReview /> },
-      { path: '/my-point', element: <MyPoint /> },
-
-      // {
-      //   path: '/products/new',
-      //   element: <NewProduct />,
-      //   children: [
-      //     { path: 'new2', element: <NewProduct2 /> }
-      //   ]
-      // },
-      // { path: '/products/:id', element: <ProductDetail /> },
-      // { path: '/carts', element: <MyCart /> },
-      // { path: '/joinForm', element: <JoinForm /> },
-      // { path: '/loginForm', element: <LoginForm /> },
     ],
   },
 ]);
--- frontend/src/pages/MyPage/MyPoint/MyPoint.jsx
+++ frontend/src/pages/MyPage/MyPoint/MyPoint.jsx
@@ -1,31 +1,82 @@
-import React, { useState } from 'react'
-import BackNav from '../../../componets/BackNav/BackNav';
-import Navbar from '../../../componets/Navbar/Navbar';
-import MyPointComponent from '../../../componets/MyPage/MyPoint/MyPoint'
+import React, { useState } from "react";
+import BackNav from "../../../componets/BackNav/BackNav";
+import Navbar from "../../../componets/Navbar/Navbar";
+import MyPointComponent from "../../../componets/MyPage/MyPoint/MyPoint";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { TbParkingCircle } from "react-icons/tb";
 
 export default function MyPoint() {
-
     // 받을 데이터
     const data = {
         userPoint: 1300,
         points: [
-            { pointDay: "2024-10-18T09:00:00", pointPlusMinus: 100, pointTitle: "예매 추가" },
-            { pointDay: "2024-10-18T10:00:00", pointPlusMinus: -100, pointTitle: "예매 사용" },
-            { pointDay: "2024-10-16T09:00:00", pointPlusMinus: -1000, pointTitle: "예매 사용" },
-            { pointDay: "2024-10-15T09:00:00", pointPlusMinus: -100, pointTitle: "예매 사용" },
-            { pointDay: "2024-09-18T09:00:00", pointPlusMinus: -100, pointTitle: "예매 사용" },
-            { pointDay: "2024-09-17T09:00:00", pointPlusMinus: 100, pointTitle: "예매 추가" },
-            { pointDay: "2024-08-17T09:00:00", pointPlusMinus: 200, pointTitle: "예매 추가" },
-            { pointDay: "2024-04-15T09:00:00", pointPlusMinus: 300, pointTitle: "예매 추가" },
-            { pointDay: "2024-03-13T09:00:00", pointPlusMinus: -100, pointTitle: "예매 사용" },
-            { pointDay: "2023-11-12T09:00:00", pointPlusMinus: -100, pointTitle: "예매 사용" },
-            { pointDay: "2023-10-11T10:00:00", pointPlusMinus: 100, pointTitle: "예매 추가" },
-            { pointDay: "2023-10-10T09:00:00", pointPlusMinus: 100, pointTitle: "예매 추가" },
-            { pointDay: "2023-10-01T09:00:00", pointPlusMinus: 1000, pointTitle: "예매 추가" },
-        ]
-    }
+            {
+                pointDay: "2024-10-18T09:00:00",
+                pointPlusMinus: 100,
+                pointTitle: "예매 추가",
+            },
+            {
+                pointDay: "2024-10-18T10:00:00",
+                pointPlusMinus: -100,
+                pointTitle: "예매 사용",
+            },
+            {
+                pointDay: "2024-10-16T09:00:00",
+                pointPlusMinus: -1000,
+                pointTitle: "예매 사용",
+            },
+            {
+                pointDay: "2024-10-15T09:00:00",
+                pointPlusMinus: -100,
+                pointTitle: "예매 사용",
+            },
+            {
+                pointDay: "2024-09-18T09:00:00",
+                pointPlusMinus: -100,
+                pointTitle: "예매 사용",
+            },
+            {
+                pointDay: "2024-09-17T09:00:00",
+                pointPlusMinus: 100,
+                pointTitle: "예매 추가",
+            },
+            {
+                pointDay: "2024-08-17T09:00:00",
+                pointPlusMinus: 200,
+                pointTitle: "예매 추가",
+            },
+            {
+                pointDay: "2024-04-15T09:00:00",
+                pointPlusMinus: 300,
+                pointTitle: "예매 추가",
+            },
+            {
+                pointDay: "2024-03-13T09:00:00",
+                pointPlusMinus: -100,
+                pointTitle: "예매 사용",
+            },
+            {
+                pointDay: "2023-11-12T09:00:00",
+                pointPlusMinus: -100,
+                pointTitle: "예매 사용",
+            },
+            {
+                pointDay: "2023-10-11T10:00:00",
+                pointPlusMinus: 100,
+                pointTitle: "예매 추가",
+            },
+            {
+                pointDay: "2023-10-10T09:00:00",
+                pointPlusMinus: 100,
+                pointTitle: "예매 추가",
+            },
+            {
+                pointDay: "2023-10-01T09:00:00",
+                pointPlusMinus: 1000,
+                pointTitle: "예매 추가",
+            },
+        ],
+    };
 
     // 현재 년도
     const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
@@ -38,23 +89,25 @@ export default function MyPoint() {
     const todayMonth = today.getMonth() + 1;
 
     // 요일 이름 배열
-    const dayNames = ['일', '월', '화', '수', '목', '금', '토'];
-
-
+    const dayNames = ["일", "월", "화", "수", "목", "금", "토"];
 
     // 월 변경 핸들러
     const changeMonth = (direction) => {
-        if (direction === 'next') {
+        if (direction === "next") {
             if (!(currentYear === todayYear && currentMonth === todayMonth)) {
-                setCurrentMonth(prevMonth => prevMonth === 12 ? 1 : prevMonth + 1);
-                if (currentMonth === 12) setCurrentYear(prevYear => prevYear + 1);
+                setCurrentMonth((prevMonth) =>
+                    prevMonth === 12 ? 1 : prevMonth + 1
+                );
+                if (currentMonth === 12)
+                    setCurrentYear((prevYear) => prevYear + 1);
             }
         } else {
-            setCurrentMonth(prevMonth => prevMonth === 1 ? 12 : prevMonth - 1);
-            if (currentMonth === 1) setCurrentYear(prevYear => prevYear - 1);
+            setCurrentMonth((prevMonth) =>
+                prevMonth === 1 ? 12 : prevMonth - 1
+            );
+            if (currentMonth === 1) setCurrentYear((prevYear) => prevYear - 1);
         }
     };
-
 
     // 선택한 월의 포인트 데이터 계산
     const getMonthlyPoints = () => {
@@ -63,60 +116,78 @@ export default function MyPoint() {
 
         // 조건에 맞는 데이터 추출함
         return data.points
-            .filter(item => {
+            .filter((item) => {
                 const pointDate = new Date(item.pointDay);
                 return pointDate >= startOfMonth && pointDate <= endOfMonth;
             })
-            .map(item => {
+            .map((item) => {
                 const date = new Date(item.pointDay);
                 const dayOfWeek = date.getDay();
                 return {
                     day: `${date.getDate()} (${dayNames[dayOfWeek]})`,
-                    fullDate: `${date.getFullYear()}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getDate().toString().padStart(2, '0')}`, // 원하는 형식으로 날짜 변환
-                    pointPlusMinus: item.pointPlusMinus > 0 ? `+${item.pointPlusMinus}` : item.pointPlusMinus, // 포인트 부호 추가
-                    pointTitle: item.pointTitle
+                    fullDate: `${date.getFullYear()}.${(date.getMonth() + 1)
+                        .toString()
+                        .padStart(2, "0")}.${date
+                        .getDate()
+                        .toString()
+                        .padStart(2, "0")}`, // 원하는 형식으로 날짜 변환
+                    pointPlusMinus:
+                        item.pointPlusMinus > 0
+                            ? `+${item.pointPlusMinus}`
+                            : item.pointPlusMinus, // 포인트 부호 추가
+                    pointTitle: item.pointTitle,
                 };
             });
     };
 
     // 버튼 비활성화 조건
-    const isPrevDisabled = (currentYear === todayYear - 1 && currentMonth === todayMonth);
-    const isNextDisabled = currentYear === todayYear && currentMonth === todayMonth;
+    const isPrevDisabled =
+        currentYear === todayYear - 1 && currentMonth === todayMonth;
+    const isNextDisabled =
+        currentYear === todayYear && currentMonth === todayMonth;
 
     return (
-        <div className='mypoint--container'>
-            <BackNav title='포인트내역' />
+        <div className="mypoint--container">
+            <BackNav title="포인트내역" />
 
-            <div className='mypoint--wrap'>
-                <div className='totalpoint'>
-                    <div className='title'>
-                        <TbParkingCircle className='icon' />
-                        <span className='text'>사용 가능한 포인트</span>
+            <div className="mypoint--wrap">
+                <div className="totalpoint">
+                    <div className="title">
+                        <TbParkingCircle className="icon" />
+                        <span className="text">사용 가능한 포인트</span>
                     </div>
 
-                    <span className='point'>1,300<span className='point--unit'>P</span></span>
+                    <span className="point">
+                        1,300<span className="point--unit">P</span>
+                    </span>
                 </div>
 
-                <div className='notify'>
-                    <span className='notifiy--text'>지난 1년간 적립/사용된 포인트 내역입니다.</span>
+                <div className="notify">
+                    <span className="notifiy--text">
+                        지난 1년간 적립/사용된 포인트 내역입니다.
+                    </span>
                 </div>
 
-                <div className='month'>
-                    <FaChevronLeft onClick={() => changeMonth('prev')} className={`icon ${isPrevDisabled ? 'disabled' : ''}`} />
+                <div className="month">
+                    <FaChevronLeft
+                        onClick={() => changeMonth("prev")}
+                        className={`icon ${isPrevDisabled ? "disabled" : ""}`}
+                    />
                     <span>{currentMonth}월</span>
-                    <FaChevronRight onClick={() => changeMonth('next')} className={`icon ${isNextDisabled ? 'disabled' : ''}`} />
+                    <FaChevronRight
+                        onClick={() => changeMonth("next")}
+                        className={`icon ${isNextDisabled ? "disabled" : ""}`}
+                    />
                 </div>
 
-                <div className='points'>
+                <div className="points">
                     {getMonthlyPoints().map((pointData, index) => (
                         <MyPointComponent key={index} pointData={pointData} />
-                    ))
-
-                    }
+                    ))}
                 </div>
             </div>
 
             <Navbar />
         </div>
-    )
+    );
 }
