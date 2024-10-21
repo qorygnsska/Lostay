61fe015bd43575363130e9ad6134acc39f15f656
jhhim <jhhim99@gmail.com>
2024-10-21 19:18:59 +0900

    객실리스트 리뷰 조회 작성중

--- backend/src/main/java/com/lostay/backend/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/lostay/backend/review/repository/ReviewRepository.java
@@ -1,9 +1,19 @@
 package com.lostay.backend.review.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.lostay.backend.review.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+	@Query("select rv from Review rv "
+			+ "Join rv.room r  "
+			+ "Join r.hotel h "
+			+ "Where h.hotelNo = :hotelNo")
+	List<Review> findHotelReview(@Param("hotelNo")long hotelNo);
 
 }
