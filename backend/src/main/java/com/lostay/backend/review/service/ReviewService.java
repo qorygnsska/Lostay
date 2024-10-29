df13ca7d95cda5737c30249dd6e8d3c4d96b4062
jh960101 <ghdgns82@gmail.com>
2024-10-29 10:47:07 +0900

    이미지 null값 수정

--- backend/src/main/java/com/lostay/backend/review/service/ReviewService.java
+++ backend/src/main/java/com/lostay/backend/review/service/ReviewService.java
@@ -275,10 +275,25 @@ public class ReviewService {
 		} else {
 			results = findReviewsByHotelNo(hotelNo, sort);
 		}
-
+		System.out.println("확인"+results.size());
+		  // results의 내용 출력
+	    System.out.println("결과 크기: " + results.size());
+	    for (Object[] result : results) {
+	        System.out.println("리뷰 번호: " + result[0]);
+	        System.out.println("리뷰 평점: " + result[1]);
+	        System.out.println("작성 시간: " + result[2]);
+	        System.out.println("사용자 닉네임: " + result[3]);
+	        System.out.println("객실 이름: " + result[4]);
+	        System.out.println("리뷰 내용: " + result[5]);
+	        System.out.println("리뷰 이미지: " + result[6]);
+	        System.out.println("---------------------------");
+	    }
+		
+		
 		List<ReviewsDTO> dtoList = new ArrayList<ReviewsDTO>();
 
 		for (Object[] d : results) {
+			System.out.println("실행ㅇㅇㅇ");
 			ReviewsDTO dto = new ReviewsDTO();
 			dto.setReviewNo((long) d[0]);
 			dto.setReviewRating((double) d[1]);
@@ -286,11 +301,16 @@ public class ReviewService {
 			dto.setUserNickname((String) d[3]);
 			dto.setRoomName((String) d[4]);
 			dto.setReviewContent((String) d[5]);
-			String[] str = d[6].toString().split(",");
-			dto.setReviewImg(str);
+			   // d[6]이 null인지 체크
+		    if (d[6] != null) {
+		        String[] str = d[6].toString().split(",");
+		        dto.setReviewImg(str);
+		    } else {
+		        dto.setReviewImg(new String[0]); // 빈 배열로 설정
+		    }
 			dtoList.add(dto);
 		}
-
+		System.out.println("확인2"+dtoList);
 		hotelInfoDTO.setHotelRoom(roomNames);
 
 		HotelReviewsDTO hotelReviewsDTO = new HotelReviewsDTO(hotelInfoDTO, dtoList);
@@ -299,8 +319,9 @@ public class ReviewService {
 	}
 	// 객실 조건 후 정렬 구분을 위한 메서드
 	private List<Object[]> findReviews(Long hotelNo, Long roomNo, String orderByColumn) {
-		
+		System.out.println("객실 조건 후 정렬 구분을 위한 메서드");
 		if (orderByColumn == null) {
+			System.out.println("객실 조건 후 정렬 구분을 위한 메서드 orderByColumn null");
 			return revRepo.findReviewsByRoomName(hotelNo, roomNo); // 기본 정렬
 		}
 		switch (orderByColumn) {
@@ -316,7 +337,9 @@ public class ReviewService {
 	}
 	//객실 조건 없이 정렬 구분을 위한 메서드
 	private List<Object[]> findReviewsByHotelNo(Long hotelNo, String orderByColumn) {
+		
 		if (orderByColumn == null) {
+			System.out.println("테스트 1");
 			return revRepo.findReviewsByRoomName(hotelNo, null); // 기본 정렬
 		}
 		switch (orderByColumn) {
