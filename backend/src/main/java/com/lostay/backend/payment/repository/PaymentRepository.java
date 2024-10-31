cc40a43779422621303c667206a66def1bc5281f
hong6425 <61792051+hong6425@users.noreply.github.com>
2024-10-31 23:49:34 +0900

    관리자 페이지 중계료 년도별, 월별 조회

--- backend/src/main/java/com/lostay/backend/payment/repository/PaymentRepository.java
+++ backend/src/main/java/com/lostay/backend/payment/repository/PaymentRepository.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.lostay.backend.payment.entity.Payment;
 
-public interface PaymentRepository extends JpaRepository<Payment, Long>{
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+	//관리자 페이지 매출액(중계료) 년도별(jh)
+	@Query("SELECT p FROM Payment p WHERE p.payStatus = 'Y'")
+	List<Payment> findAllSuccessfulPayments();
 
-	
+	//관리자 페이지 매출액(중계료) 월별(jh)
+	@Query("SELECT p FROM Payment p WHERE p.payStatus = 'Y' AND YEAR(p.payDay) = :year")
+	List<Payment> findSuccessfulPaymentsByYear(@Param("year") int year);
+
 }
