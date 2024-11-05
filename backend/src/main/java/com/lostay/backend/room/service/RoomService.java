1210a255949d4e8e6b5743bbe9bc39eb7e7b5479
Hangma01 <dn09yooon@gmail.com>
2024-11-05 15:35:22 +0900

    효준 결제내역 수정

--- backend/src/main/java/com/lostay/backend/room/service/RoomService.java
+++ backend/src/main/java/com/lostay/backend/room/service/RoomService.java
@@ -36,51 +36,43 @@ public class RoomService {
 
 	@Autowired
 	private RoomRepository roomRepo;
-	
+
 	@Autowired
 	private ReviewRepository revRepo;
 
-	
 	@Autowired
 	private HotelRepository hotelRepo;
-	
+
 	@Autowired
 	private RoomRedisRepository roomRedisRepo;
-	
+
 	@Autowired
 	private RedisTemplate<String, RoomCheck> redisTemplate;
-	
 
 	// 호텔에 대한 객실 리스트 조회(호텔과 객실 정보)
-	public RoomListDTO findHotelRoomList(Long hotelNo, LocalDateTime checkInDate, LocalDateTime checkOutDate, int peopleMax) {
+	public RoomListDTO findHotelRoomList(Long hotelNo, LocalDateTime checkInDate, LocalDateTime checkOutDate,
+			int peopleMax) {
 
-		LocalDateTime in = checkInDate.toLocalDate().atTime(15,0);
-		LocalDateTime out = checkOutDate.toLocalDate().atTime(11,0);
-		
+		LocalDateTime in = checkInDate.toLocalDate().atTime(15, 0);
+		LocalDateTime out = checkOutDate.toLocalDate().atTime(11, 0);
+
 		long period = ChronoUnit.DAYS.between(checkInDate.toLocalDate(), checkOutDate.toLocalDate());
 //		Period period = Period.between(checkInDate.toLocalDate(), checkOutDate.toLocalDate());
-		
+
 //		List<Object[]> newRoom = roomRepo.findHotelRoomList(hotelNo, in, out);
 		RoomListHotelInfoDTO Hoteldto = roomRepo.findHotelInfo(hotelNo);
-		List<RoomCustomDTO> list = roomRepo.findRoomCumstomList(hotelNo,in,out);
-		
+		List<RoomCustomDTO> list = roomRepo.findRoomCumstomList(hotelNo, in, out);
+
 //		List<RoomDTO> dtos = new ArrayList<RoomDTO>();
 //		
 //		int reviewCount = revRepo.findHotelReviewCount(hotelNo);
 //		Double ReviewAvg = revRepo.findHotelReviewAvg(hotelNo);
 
-		
 //		if (ReviewAvg == null) {
 //			ReviewAvg = 0.0;
 //		}
-		
-		RoomListDTO dto = new RoomListDTO(
-				Hoteldto
-			   ,in.toLocalDate()
-			   ,out.toLocalDate()
-			   ,period
-			   ,list	
-				);
+
+		RoomListDTO dto = new RoomListDTO(Hoteldto, in.toLocalDate(), out.toLocalDate(), period, list);
 ////		
 //		dto.setPeriod(period.getDays());
 //		dto.setCheckInDay(in.toLocalDate());
@@ -89,38 +81,35 @@ public class RoomService {
 ////		dto.setTotalReviewCount(reviewCount);
 //		dto.setList(list);
 //		
-			System.out.println("테스트"+dto.getDto().getHotelThumbnail());
-		
+		System.out.println("테스트" + dto.getDto().getHotelThumbnail());
+
 		return dto;
 	}
 
+	// 해당 객실에 대한 정보 조회
+	public RoomDTO findRoomInfo(long roomNo, LocalDateTime checkInDate, LocalDateTime checkOutDate, int peopleMax) {
 
-	
-	//  해당 객실에 대한 정보 조회
-	public RoomDTO findRoomInfo(long roomNo,LocalDateTime checkInDate, LocalDateTime checkOutDate, int peopleMax) {
-		
-		
 		Optional<Room> newRoom2 = roomRepo.findById(roomNo);
 		Room room2 = newRoom2.get();
-		
+
 		long period = ChronoUnit.DAYS.between(checkInDate.toLocalDate(), checkOutDate.toLocalDate());
-		
+
 		Long hotelNo = room2.getHotel().getHotelNo();
-		
-		List<RoomCustomDTO> list = roomRepo.findRoomCumstomList(hotelNo,checkInDate,checkOutDate);
+
+		List<RoomCustomDTO> list = roomRepo.findRoomCumstomList(hotelNo, checkInDate, checkOutDate);
 		Long avc = 0L;
-		
-		for(int i = 0; i < list.size(); i++) {
-			if(list.get(i).getRoomNo() == roomNo) {
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getRoomNo() == roomNo) {
 				avc = list.get(i).getAvailableRooms();
 			}
 		}
 		Optional<Room> newRoom = roomRepo.findById(roomNo);
-		
+
 		Room room = newRoom.get();
-		
+
 		RoomDTO dto = new RoomDTO();
-		
+
 		dto.setRoomNo(room.getRoomNo());
 		dto.setRoomName(room.getRoomName());
 		dto.setHotelNo(room.getHotel().getHotelNo());
@@ -133,7 +122,7 @@ public class RoomService {
 		dto.setRoomDiscount(room.getRoomDiscount());
 		int roomPrice = room.getRoomPrice();
 		int roomDiscount = room.getRoomDiscount();
-		int discountPrice = (int)roomPrice - roomPrice * roomDiscount/100;
+		int discountPrice = (int) roomPrice - roomPrice * roomDiscount / 100;
 		dto.setDiscountPrice(discountPrice);
 		dto.setCheckInDay(checkInDate.toLocalDate());
 		dto.setCheckOutDay(checkOutDate.toLocalDate());
@@ -144,23 +133,19 @@ public class RoomService {
 		dto.setAvailableRooms(avc);
 		dto.setPeriod(period);
 		dto.setRoomThumbnail(room.getRoomThumbnail());
-	
+
 		return dto;
 	}
 
-
-
-	
 	public Long findAvailableCount(RoomCheckDTO dto) {
-		
-		LocalDateTime in = dto.getCheckInDay().atTime(15,0,0);
-	    LocalDateTime out = dto.getCheckOutDay().atTime(11,0,0);
-	    Long roomNo = dto.getRoomNo();
-	    Long count = roomRepo.findAvailableCount(roomNo,in,out);
 
-	    return count;
+		LocalDateTime in = dto.getCheckInDay().atTime(15, 0, 0);
+		LocalDateTime out = dto.getCheckOutDay().atTime(11, 0, 0);
+		Long roomNo = dto.getRoomNo();
+		Long count = roomRepo.findAvailableCount(roomNo, in, out);
+
+		return count;
 	}
-
 
 //    // 레디스에 값이 있는지 찾아오는거
 //	public RoomCheckDTO findRedisInfo(Long roomNo,LocalDateTime in,LocalDateTime out) {
@@ -173,84 +158,73 @@ public class RoomService {
 //		
 //	}
 
-
-
 	// 업데이트 및 저장
 	public RoomCheckDTO RedisSave(Long roomNo, LocalDate in, LocalDate out) {
 
-		
-		RoomCheck ch = new RoomCheck(); 
-			
-			ch.setRoomNo(roomNo);
-			ch.setCheckInDay(in);
-			ch.setCheckOutDay(out);
-			
-			
-			roomRedisRepo.save(ch);
-			
+		RoomCheck ch = new RoomCheck();
 
+		ch.setRoomNo(roomNo);
+		ch.setCheckInDay(in);
+		ch.setCheckOutDay(out);
 
-		RoomCheckDTO c1 = new RoomCheckDTO(ch.getRid()
-						,ch.getRoomNo(),ch.getCheckInDay(),ch.getCheckOutDay());
-		
+		roomRedisRepo.save(ch);
+
+		RoomCheckDTO c1 = new RoomCheckDTO(ch.getRid(), ch.getRoomNo(), ch.getCheckInDay(), ch.getCheckOutDay());
+
 		return c1;
 	}
 
-	
 	// 레디스에서 roomCheck:123214354738483598327984 통해서 값 가져오기
 	public Optional<RoomCheck> getRoomCheck(String rid) {
-		 HashOperations<String, String, RoomCheck> hashOperations = redisTemplate.opsForHash();
-	        RoomCheck roomCheck = hashOperations.get("roomCheck", rid);
-	        return Optional.ofNullable(roomCheck);
-    }
-	
+		HashOperations<String, String, RoomCheck> hashOperations = redisTemplate.opsForHash();
+		RoomCheck roomCheck = hashOperations.get("roomCheck", rid);
+		return Optional.ofNullable(roomCheck);
+	}
+
 	// redis에 해당하는 값을 가진 사람이 몇명 있는지
 	public int findRedisHumanCount(RoomCheckDTO dto) {
-		 String ping = redisTemplate.getConnectionFactory().getConnection().ping();
-		    System.out.println("Redis connection: " + ping); // true가 출력되어야 함
-		    
-		  Set<String> keys = redisTemplate.keys("*");
-		  List<String> redisKey = new ArrayList<String>();
-		  for (String value : keys) {
-			  if(value.length() > 9){
-			  if(value.substring(0,10).equals("roomCheck:")) {
-				  redisKey.add(value.substring(10));
-				  System.out.println("keys : " + value);
-			    
-			}
-		  }
-	   }	  
-	
-		  
-		  int totalCount = 0;
-		  
-		  
-		  if (redisKey != null) {
-		        for (String key : redisKey) {
-		        	System.out.println("여기서 출력되니?" + key.toString());
-		        	
-		        	  System.out.println("이 부분 왜 출력이 안되는거야? : "  + key);
-		        	  System.out.println("총 카운트가 몇이야 ? " + totalCount);
-		        	  Optional<RoomCheck> roomCheck = roomRedisRepo.findById(Long.parseLong(key));
-		        	  if (roomCheck != null) {
-		                  // 각 필드를 가져오기 (예: "roomNo", "checkInDay", "checkOutDay")
-		                  Long roomNo = roomCheck.get().getRoomNo();
-		                  LocalDate checkInDay = (roomCheck.get().getCheckInDay());
-		                  LocalDate checkOutDay = (roomCheck.get().getCheckOutDay());
+		String ping = redisTemplate.getConnectionFactory().getConnection().ping();
+		System.out.println("Redis connection: " + ping); // true가 출력되어야 함
 
-		                  // 비교 로직
-		                  if (roomNo.equals(dto.getRoomNo()) &&
-		                      checkInDay.isBefore(dto.getCheckOutDay()) &&
-		                      checkOutDay.isAfter(dto.getCheckInDay())) {
-		                      totalCount += 1;
-		                  }
-		        	  }
-		        }
-		    }else {
-		    	System.out.println("키가 널인거야? 말이 안되는데");
-		    }
-		
+		Set<String> keys = redisTemplate.keys("*");
+		List<String> redisKey = new ArrayList<String>();
+		for (String value : keys) {
+			if (value.length() > 9) {
+				if (value.substring(0, 10).equals("roomCheck:")) {
+					redisKey.add(value.substring(10));
+					System.out.println("keys : " + value);
+
+				}
+			}
+		}
+
+		int totalCount = 0;
+
+		if (redisKey != null) {
+			for (String key : redisKey) {
+				System.out.println("여기서 출력되니?" + key.toString());
+
+				System.out.println("이 부분 왜 출력이 안되는거야? : " + key);
+				System.out.println("총 카운트가 몇이야 ? " + totalCount);
+				Optional<RoomCheck> roomCheck = roomRedisRepo.findById(Long.parseLong(key));
+				if (roomCheck != null) {
+					// 각 필드를 가져오기 (예: "roomNo", "checkInDay", "checkOutDay")
+					Long roomNo = roomCheck.get().getRoomNo();
+					LocalDate checkInDay = (roomCheck.get().getCheckInDay());
+					LocalDate checkOutDay = (roomCheck.get().getCheckOutDay());
+
+					// 비교 로직
+					if (roomNo.equals(dto.getRoomNo()) && checkInDay.isBefore(dto.getCheckOutDay())
+							&& checkOutDay.isAfter(dto.getCheckInDay())) {
+						totalCount += 1;
+					}
+				}
+			}
+		} else {
+			System.out.println("키가 널인거야? 말이 안되는데");
+		}
+
 		return totalCount;
 	}
-	
+
 }
--- frontend/src/componets/RoomNav/RoomNav.jsx
+++ frontend/src/componets/RoomNav/RoomNav.jsx
@@ -3,28 +3,28 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
 
-export default function RoomNav({info}) {
+export default function RoomNav({ info }) {
 
     const price = info.period * info.discountPrice;
 
-  return (
-    <div className='room--nav--container'>
-        <div className='roomDiv'>
-            <div>
-                <div className='infoName'>{info.hotelName} - {info.roomName}</div>
-                <div className='NavDate'>{info.checkInDay} ~ {info.checkOutDay} ({info.period}박)</div>
-            </div>
+    return (
+        <div className='room--nav--container'>
+            <div className='roomDiv'>
+                <div>
+                    <div className='infoName'>{info.hotelName} - {info.roomName}</div>
+                    <div className='NavDate'>{info.checkInDay} ~ {info.checkOutDay} ({info.period}박)</div>
+                </div>
 
-            <div className='PriceBox'>
-                <div className='Price'>{price.toLocaleString()}원</div>
-                {info.availableRooms == 0 ? (
-                    <Button className='navSBtn' disabled>예약마감</Button>
-                ) : (
-                    <Link><Button className='navBtn'>예약하기</Button></Link>
-                )}
-                
+                <div className='PriceBox'>
+                    <div className='Price'>{price.toLocaleString()}원</div>
+                    {info.availableRooms == 0 ? (
+                        <Button className='navSBtn' disabled>예약마감</Button>
+                    ) : (
+                        <Link to={`/reservation?roomNo=${info.roomNo}`}><Button className='navBtn'>예약하기</Button></Link>
+                    )}
+
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
